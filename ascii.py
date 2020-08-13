58328e8dabe8397558583b2797a3fb1e3799f20d
Benedikt Müller <65851227+Ben-bittec@users.noreply.github.com>
2020-08-13 21:05:56 +0200

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,16 @@
+import pyfiglet
+
+name=input("Enter A Text: ")
+name=str(name)
+while True:
+    try:
+        style=input("Enter Style: ")
+        style=str(style)
+        word=pyfiglet.figlet_format(name,font=style)
+        print(word)
+    except Exception as e:
+    	print("Wrong Font Style")
+    	style=input("Enter Style: ")
+    	style=str(style)
+    	word=pyfiglet.figlet_format(name,font=style)
+    	print(word)
